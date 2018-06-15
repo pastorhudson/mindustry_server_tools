1d58c8abc0e172fa5e2966755c57b6e5d41693d8
Ron Hudson <pastorhudson@users-iMac-3.local>
2018-06-15 16:47:03 -0400

    first upload

---
+++ refreshmaps.py
@@ -0,0 +1,28 @@
+import json
+import glob
+import os
+
+with open('maps.json') as json_file:
+    data = json.load(json_file)
+with open('maps.bak', 'w') as outfile:
+    json.dump(data, outfile)
+
+dirMaps = ([os.path.basename(x[:-4]) for x in glob.glob('*.png')])
+minKey = min(g['id'] for g in data['maps'])
+newMapJson = {'maps': []}
+
+print("Rebuilding map list with these maps:")
+
+for p in dirMaps:
+    print(p)
+    map = {'id':(minKey), 'name':p, 'custom':True}
+    newMapJson['maps'].append(map)
+    minKey = minKey+1
+
+
+
+
+with open('maps.json', 'w') as outfile:
+    json.dump(newMapJson, outfile)
+print ("Done!")
+print ("Don't forget to restart the server to load the new maps.")
