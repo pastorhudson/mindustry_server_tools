903b8aeeba886b8920a31faffa2c3d8680235119
pastorhudson@gmail.com <emerg07ams>
2018-06-17 19:54:33 -0400

    Cleaner ping response

--- cogs/mindustry.py
+++ cogs/mindustry.py
@@ -20,8 +20,7 @@ class MindustryCog:
             dreply = base64.b64decode(reply)
             embed = discord.Embed(title=server,
                                   colour=0x33CCFF)
-            embed.set_author(name=self.bot.user.display_name,
-                            icon_url=self.bot.user.avatar_url_as(format='png'))
+
 
             embed.add_field(name='Host',
                             value=str(dreply[1:(dreply[0]+1)])[2:(dreply[0]+2)])
@@ -29,8 +28,7 @@ class MindustryCog:
                             value=f'{str(dreply[(dreply[0]+2):(dreply[0]+2+dreply[dreply[0]+1])])[2:(dreply[0]+dreply[dreply[0]+1])]}')
             embed.add_field(name='Players',
                             value=dreply[dreply[0]+5+dreply[dreply[0]+1]])
-            embed.set_footer(text=ctx.guild.name,
-                            icon_url=ctx.guild.icon_url_as(format='png'))
+
             await ctx.send(embed=embed)
         
 # The setup fucntion below is neccesarry. Remember we give bot.add_cog() the name of the class in this case MembersCog.
