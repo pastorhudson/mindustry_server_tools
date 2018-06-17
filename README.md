4b81eddb4f80b52d3d3ceda47a490a11cddfdf43
pastorhudson@gmail.com <emerg07ams>
2018-06-17 19:42:45 -0400

    Rewritten to use discord.py rewrite and cogs.

--- README.md
+++ README.md
@@ -1,9 +1,24 @@
 ## mindustry_server_tools
 Tools for administering a headless Mindustry server
+
+###refreshmaps.py
 A simple python script that scans the mindustry-maps folder and adds any new png files to the server map list.
-### Installing
+
+#### Installing
 Copy refreshmaps.py to your server's mindustry-maps folder.
-### Adding And Removing Maps
+#### Adding And Removing Maps
 Simply add or remove .png maps to the mindustry-maps folder and run the script.
 python refreshmaps.py
 
+###mindustry_server_discord_bot.py
+Mindustry discord bot based on https://github.com/Greenfoot5/GreenBOT and my own added cogs.
+
+### Installing
+You need python 3.6 and discord.py rewrite
+
+* Install the discord.py rewrite using:
+'''sudo python3.6 -m pip install -U git+https://github.com/Rapptz/discord.py@rewrite#egg=discord.py[voice]'''
+* Git clone this repository
+* Put your token in token.txt in the mindustry_server_tools folder
+* Set your mindustry-maps folder location in /cogs/maps.py
+* Run with ''' python3.6 mindustry_discord_bot.py
--- mindustry_server_discord_bot.py
+++ mindustry_server_discord_bot.py
@@ -1,31 +1,71 @@
 import discord
-import json
-
 from discord.ext import commands
+import sys, traceback
 
-description = '''A simple discord bot using discord.py that lists the maps in maps.json. 
-This lets players know what custom maps are currently loaded on the server'''
+"""These examples make use of Python 3.6.2 and the rewrite version on the lib.
 
-#setup bot object and listen for !command
-bot = commands.Bot(command_prefix='!', description=description)
+For examples on cogs for the async version:
+https://gist.github.com/leovoel/46cd89ed6a8f41fd09c5
 
-#login stuff
+Rewrite Documentation:
+http://discordpy.readthedocs.io/en/rewrite/api.html
+
+Rewrite Commands Documentation:
+http://discordpy.readthedocs.io/en/rewrite/ext/commands/api.html
+
+Familiarising yourself with the documentation will greatly help you in creating your bot and using cogs.
+
+Thank you to GreenBOT https://github.com/Greenfoot5/GreenBOT
+I couldn't have figured this out with them! Much of the code here borrows from GreenBOT
+"""
+
+with open('token.txt', 'r') as tokenfile:
+    token=tokenfile.read().replace('\n', '')
+
+def get_prefix(bot, message):
+    """A callable Prefix for our bot. This could be edited to allow per server prefixes."""
+
+    # Can use spaces. Keep simple though.
+    prefixes = ['!']
+
+    # Check to see if we are outside of a guild. e.g DM's etc.
+    if not message.guild:
+        # Only allow this prefix to be used in DMs
+        return ['!']
+
+    # If we are in a guild, we allow for the user to mention us or use any of the prefixes in our list.
+    return commands.when_mentioned_or(*prefixes)(bot, message)
+
+
+# Below cogs represents our folder our cogs are in. Following is the file name. So 'meme.py' in cogs, would be cogs.meme
+# Think of it like a dot path import
+initial_extensions = ['cogs.help',
+                      'cogs.mindustry',
+                      'cogs.maps']
+
+
+bot = commands.Bot(command_prefix=get_prefix, description="A bot for mindustry.pastorhudson.com", self_bot=False)
+bot.remove_command('help')
+
+# Here we load our extensions(cogs) listed above in [initial_extensions].
+if __name__ == '__main__':
+    for extension in initial_extensions:
+        try:
+            bot.load_extension(extension)
+        except Exception as e:
+            print(f'Failed to load extension {extension}.', file=sys.stderr)
+            traceback.print_exc()
+
+
 @bot.event
 async def on_ready():
-    print('Logged in as')
-    print(bot.user.name)
-    print(bot.user.id)
-    print('------')
+    """http://discordpy.readthedocs.io/en/rewrite/api.html#discord.on_ready"""
 
+    print(f'\n\nLogged in as: {bot.user.name} - {bot.user.id}\nMindustry Server Bot\n')
 
-#when !maps is recieved run this:
-@bot.command()
-async def maps():
-    #Open the maps.json file
-    with open('maps.json') as json_file:
-        data = json.load(json_file)
-        #Send the ['maps'] ['name'] to the discord channel
-        await bot.say("**Currently Loaded Maps:** \n" + "```" + "\n".join(x.get("name", "") for x in data["maps"]) + "```")
+    #pickle.dump([['a',5,2,5]], open('XP.data','wb'))
+    # Changes our bots Playing Status. type=1(streaming) for a standard game you could remove type and url.
+    await bot.change_presence(activity=discord.Activity(name="!help",type=0))
+    print(f'Successfully logged in and booted...!\n')
 
-#Runs the Bot
-bot.run('Put your token here')
+bot.run(token, bot=True, reconnect=True)
