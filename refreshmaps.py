6d35f68394a6360b743b0d0d209a58da8bebb99f
pastorhudson@gmail.com <emerg07ams>
2018-06-21 22:36:32 -0400

    maps.json is created if none exists. Oregen off is handled by filename. Json is prettier.

--- refreshmaps.py
+++ refreshmaps.py
@@ -1,25 +1,44 @@
 import json
 import glob
 import os
+from collections import OrderedDict
 
-with open('maps.json') as json_file:
-    data = json.load(json_file)
-with open('maps.bak', 'w') as outfile:
-    json.dump(data, outfile)
+minKey = 13
+try:
+    with open('maps.json') as json_file:
+        data = json.load(json_file, object_pairs_hook=OrderedDict)
+        print('maps.json found!')
 
-dirMaps = ([os.path.basename(x[:-4]) for x in glob.glob('*.png')])
-minKey = min(g['id'] for g in data['maps'])
-newMapJson = {'maps': []}
+except:
+    print("No maps.json found. One will be created.")
+    data = {"maps":[{"id":13,"name":"TEST","custom":True,"oreGen":True}]}
 
-print("Rebuilding map list with these maps:")
+else:
+        minKey = min(g['id'] for g in data['maps'])
 
-for p in dirMaps:
-    print(p)
-    map = {'id':(minKey), 'name':p, 'custom':True}
-    newMapJson['maps'].append(map)
-    minKey = minKey+1
+finally:
+    print('Scanning maps folder. . .')
+    print('Renaming maps to lowercase. . .')
+    for x in glob.glob('*.png'):
+        os.rename(src=x,dst=x.lower())
+    print('Done.')
+    dirMaps = ([os.path.basename(x[:-4]) for x in glob.glob('*.png')])
+    newMapJson = {'maps': []}
+    print("Rebuilding map list with these maps:")
 
-with open('maps.json', 'w') as outfile:
-    json.dump(newMapJson, outfile)
-print ("Done!")
-print ("Don't forget to restart the server to load the new maps.")
+    for p in dirMaps:
+        dirMaps = ([os.path.basename(x[:-4]) for x in glob.glob('*.png')])
+        if 'oregenoff' in p:
+            map = {'id': (minKey), 'name': p, 'custom': True, 'oreGen': False}
+        else:
+            map = {'id':(minKey), 'name':p, 'custom':True,'oreGen':True}
+        print(p)
+        newMapJson['maps'].append(map)
+        minKey = minKey+1
+
+    #[value for key, value in programs.items() if 'new york' in key.lower()]
+
+    with open('maps.json', 'w') as outfile:
+        json.dump(newMapJson, outfile, indent=4)
+    print ("\nDone!")
+    print ("Don't forget to restart the server to load the new maps.")
